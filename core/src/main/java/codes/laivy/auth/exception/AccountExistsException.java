c052ac59cf03ac725a2f3bcecd1e72f78f1a48f5
Daniel Richard <dnlfg.contato@gmail.com>
2024-12-21 15:58:22 -0300

    removed old files

--- core/src/main/java/codes/laivy/auth/exception/AccountExistsException.java
+++
@@ -1,9 +0,0 @@
-package codes.laivy.auth.exception;
-
-import org.jetbrains.annotations.NotNull;
-
-public final class AccountExistsException extends Exception {
-    public AccountExistsException(@NotNull String message) {
-        super(message);
-    }
-}
