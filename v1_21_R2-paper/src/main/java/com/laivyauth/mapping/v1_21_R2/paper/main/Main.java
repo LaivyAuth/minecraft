aa0e88876d023171028e0e8a0b8ce0c74e4e7495
Daniel Richard <dnlfg.contato@gmail.com>
2025-02-01 00:59:01 -0300

    added v1_21_R2-paper mapping

---
+++ v1_21_R2-paper/src/main/java/com/laivyauth/mapping/v1_21_R2/paper/main/Main.java
@@ -0,0 +1,148 @@
+package com.laivyauth.mapping.v1_21_R2.paper.main;
+
+import com.laivyauth.api.LaivyAuthApi;
+import com.laivyauth.api.config.Configuration;
+import com.laivyauth.api.mapping.Mapping;
+import com.laivyauth.api.platform.Platform;
+import com.laivyauth.api.platform.Version;
+import com.laivyauth.mapping.exception.ExceptionHandler;
+import com.laivyauth.mapping.impl.ConnectionImpl;
+import com.laivyauth.mapping.netty.NettyInjection;
+import com.laivyauth.mapping.v1_21_R2.paper.PaperInjection;
+import net.minecraft.SharedConstants;
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.CraftServer;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.UnknownNullability;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+
+public final class Main implements Mapping {
+
+    // Static initializers
+
+    private static @UnknownNullability Main instance;
+    public static final @NotNull Logger log = LoggerFactory.getLogger(name());
+
+    private static @NotNull String name() {
+        return "1.21.2/3 Paper";
+    }
+
+    // Object
+
+    private final @NotNull ClassLoader classLoader;
+    private final @NotNull LaivyAuthApi api;
+    private final @NotNull Configuration configuration;
+
+    private final @NotNull ExceptionHandler exceptionHandler;
+    private @Nullable NettyInjection injection;
+
+    private Main(@NotNull ClassLoader classLoader, @NotNull LaivyAuthApi api, @NotNull Configuration configuration) {
+        // Variables
+        this.classLoader = classLoader;
+        this.api = api;
+        this.configuration = configuration;
+
+        this.exceptionHandler = new ExceptionHandler(api.getVersion(), new File(api.getDataFolder(), "exceptions/"));
+
+        // Instance
+        Main.instance = this;
+    }
+
+    // Getters
+
+    @Override
+    public @NotNull Collection<ConnectionImpl> getConnections() {
+        return ConnectionImpl.retrieve();
+    }
+
+    @Override
+    public @NotNull ClassLoader getClassLoader() {
+        return classLoader;
+    }
+    public static @NotNull LaivyAuthApi getApi() {
+        return instance.api;
+    }
+    public static @NotNull Configuration getConfiguration() {
+        return instance.configuration;
+    }
+
+    public static @NotNull ExceptionHandler getExceptionHandler() {
+        return instance.exceptionHandler;
+    }
+
+    // Mapping
+
+    @Override
+    public @NotNull String getName() {
+        return name();
+    }
+
+    @Override
+    public @NotNull String getVendor() {
+        return "LaivyAuth (https://laivyauth.com/)";
+    }
+
+    @Override
+    public @NotNull Platform @NotNull [] getPlatforms() {
+        return new Platform[] { Platform.PAPER };
+    }
+
+    @Override
+    public @NotNull Version getVersion() {
+        return Version.create(1, 0); // 1.0
+    }
+
+    public int @NotNull [] getCompatibleVersions() {
+        return new int[] { 768 };
+    }
+    @Override
+    public boolean isCompatible() {
+        try {
+            // Check by compatible platforms
+            if (Arrays.stream(getPlatforms()).noneMatch(Platform::isCompatible)) {
+                return false;
+            }
+
+            // Retrieve the protocol version
+            int protocol = SharedConstants.getProtocolVersion();
+
+            // Finish
+            return Arrays.stream(getCompatibleVersions()).anyMatch(compatible -> compatible == protocol);
+        } catch (@NotNull Throwable throwable) {
+            return false;
+        }
+    }
+
+    // Loaders
+
+    @Override
+    public void start() {
+        if (Platform.PAPER.isCompatible()) {
+            // Configure online-mode
+            if (getConfiguration().getPremiumAuthentication().isEnabled()) {
+                // Set 'online-mode' to true
+                ((CraftServer) Bukkit.getServer()).getServer().setUsesAuthentication(true);
+            }
+
+            // Start injection
+            injection = new PaperInjection();
+        } else {
+            throw new UnsupportedOperationException();
+        }
+    }
+    @Override
+    public void close() throws IOException {
+        if (injection != null) {
+            injection.flush();
+            injection = null;
+        }
+    }
+
+}
---
+++ v1_21_R2-paper/src/main/java/com/laivyauth/mapping/v1_21_R2/paper/reflections/Reflections.java
@@ -0,0 +1,119 @@
+package com.laivyauth.mapping.v1_21_R2.paper.reflections;
+
+import com.mojang.authlib.GameProfile;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import net.minecraft.network.Connection;
+import net.minecraft.server.network.ServerConnectionListener;
+import net.minecraft.server.network.ServerHandshakePacketListenerImpl;
+import net.minecraft.server.network.ServerLoginPacketListenerImpl;
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.CraftServer;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.InetAddress;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public final class Reflections {
+
+    // Static initializers
+
+    public static @NotNull Channel getServerChannel() {
+        try {
+            // Retrieve server's connection
+            @NotNull ServerConnectionListener connection = ((CraftServer) Bukkit.getServer()).getServer().getConnection();
+
+            // Retrieve channel futures
+            @NotNull Field field = ServerConnectionListener.class.getDeclaredField("channels");
+            field.setAccessible(true);
+
+            //noinspection unchecked
+            @NotNull List<ChannelFuture> list = (List<ChannelFuture>) field.get(connection);
+
+            //noinspection SequencedCollectionMethodCanBeUsed
+            return list.get(0).channel();
+        } catch (@NotNull NoSuchFieldException e) {
+            throw new RuntimeException("cannot find channels field", e);
+        } catch (@NotNull IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void finish(@NotNull ServerLoginPacketListenerImpl listener, @NotNull GameProfile profile) {
+        try {
+            @NotNull Method method = ServerLoginPacketListenerImpl.class.getDeclaredMethod("callPlayerPreLoginEvents", GameProfile.class);
+            method.setAccessible(true);
+
+            method.invoke(listener, profile);
+
+            // Mark login state
+            method = ServerLoginPacketListenerImpl.class.getDeclaredMethod("startClientVerification", GameProfile.class);
+            method.setAccessible(true);
+
+            method.invoke(listener, profile);
+        } catch (@NotNull NoSuchMethodException e) {
+            throw new RuntimeException("cannot find proper login finish methods", e);
+        } catch (@NotNull IllegalAccessException e) {
+            throw new RuntimeException("cannot invoke login finish methods", e);
+        } catch (@NotNull InvocationTargetException e) {
+            throw new RuntimeException("exception trying to invoke player login finish methods", e);
+        }
+    }
+    public static @NotNull Optional<Connection> getNetworkManager(@NotNull Channel channel) {
+        // Retrieve server connection
+        @Nullable ServerConnectionListener connection = ((CraftServer) Bukkit.getServer()).getServer().getConnection();
+        return connection.getConnections().stream().filter(network -> channel.equals(network.channel)).findFirst();
+    }
+    public static byte[] getEncryptionBytes(@NotNull ServerLoginPacketListenerImpl listener) {
+        try {
+            @NotNull Field field = listener.getClass().getDeclaredField("challenge");
+            field.setAccessible(true);
+
+            return (byte[]) field.get(listener);
+        } catch (@NotNull NoSuchFieldException e) {
+            throw new RuntimeException("cannot get encryption bytes from login listener", e);
+        } catch (@NotNull IllegalAccessException e) {
+            throw new RuntimeException();
+        }
+    }
+    public static void resetThrottling(@NotNull InetAddress address) {
+        try {
+            // Retrieve throttle tracker's map instance
+            @NotNull Field field = ServerHandshakePacketListenerImpl.class.getDeclaredField("throttleTracker");
+            field.setAccessible(true);
+
+            //noinspection unchecked
+            @NotNull Map<InetAddress, Long> map = (Map<InetAddress, Long>) field.get(null);
+
+            // Remove from map
+            map.remove(address);
+        } catch (@NotNull NoSuchFieldException | @NotNull IllegalAccessException e) {
+            throw new RuntimeException("cannot retrieve/access throttle tracker field from handshake listener class", e);
+        }
+    }
+    public static @NotNull GameProfile initializeUniqueId(@NotNull ServerLoginPacketListenerImpl listener, @NotNull String name) {
+        try {
+            @NotNull Method initializeMethod = ServerLoginPacketListenerImpl.class.getDeclaredMethod("createOfflineProfile", String.class);
+            initializeMethod.setAccessible(true);
+
+            return (GameProfile) initializeMethod.invoke(listener, name);
+        } catch (@NotNull NoSuchMethodException e) {
+            throw new RuntimeException("cannot find uuid initialization method", e);
+        } catch (@NotNull InvocationTargetException | @NotNull IllegalAccessException e) {
+            throw new RuntimeException("cannot invoke or access uuid initialization method", e);
+        }
+    }
+
+    // Object
+
+    private Reflections() {
+        throw new UnsupportedOperationException("this class cannot be instantiated");
+    }
+
+}
